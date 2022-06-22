11ed601c358f319b6425854d05a32fe093881fce
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-06-22 13:30:13 +0300

    add parallax scroll effect

--- ParallaxScrollEffect/app/src/main/java/com/nisaefendioglu/parallaxscrolleffect/MainActivity.kt
+++ ParallaxScrollEffect/app/src/main/java/com/nisaefendioglu/parallaxscrolleffect/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,28 +18,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ParallaxScrollEffectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            //ParallaxEffect fonk kullanmak için scroll yapma durumunu tanımlıyoruz.
+            val scrollState = rememberScrollState()
+            ParallaxScrollEffectTheme() {
+                Surface(color = MaterialTheme.colors.background) {
+                    Scaffold() {
+                        ParallaxEffect(scrollState)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ParallaxScrollEffectTheme {
-        Greeting("Android")
     }
 }
---
+++ ParallaxScrollEffect/app/src/main/java/com/nisaefendioglu/parallaxscrolleffect/ParallaxEffect.kt
@@ -0,0 +1,77 @@
+package com.nisaefendioglu.parallaxscrolleffect
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.*
+import kotlin.math.*
+
+@Composable
+fun ParallaxEffect(scrollState: ScrollState) {
+    val text = "Android software development is the process by which applications are created for devices running the Android operating system. Google states that[3] \"Android apps can be written using Kotlin, Java, and C++ languages\" using the Android software development kit (SDK), while using other languages is also possible. All non-Java virtual machine (JVM) languages, such as Go, JavaScript, C, C++ or assembly, need the help of JVM language code, that may be supplied by tools, likely with restricted API support. Some programming languages and tools allow cross-platform app support (i.e. for both Android and iOS). Third party tools, development environments, and language support have also continued to evolve and expand since the initial SDK was released in 2008. The official Android app distribution mechanism to end users is Google Play; it also allows staged gradual app release, as well as distribution of pre-release app versions to testers."
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+            TopAppBar(
+                //TopAppBar alanındaki başlık stili
+                title = {
+                    Text(
+                        text = "Android Software Development",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            )
+
+            Column(
+                //Y ekseninde scrolling yapabilmek için verticalScroll tanımlama
+                modifier = Modifier
+                    .verticalScroll(scrollState)
+                    .fillMaxSize()
+            ) {
+                val height = 320.dp
+                Image(
+                    //Kullanılan resmi belirleme
+                    painter = painterResource(id = R.drawable.android),
+                    contentDescription = "Android Image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(height)
+                        .graphicsLayer {
+                            //Scroll yapma işleminde soluklaştırma görüntüsünü oluşturma
+                            alpha = min(
+                                1f,
+                                max(
+                                    0.0f,
+                                    1 - (scrollState.value / ((height.value * 2) + (height.value / 1.5f)))
+                                )
+                            )
+                        }
+                )
+                //İçerik yazısıyla ilgili still özelliklerini tanımlama
+                Text(
+                    text = text,
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.SansSerif,
+                    modifier = Modifier.padding(20.dp),
+                    style = TextStyle(
+                        lineHeight = 30.sp,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+        }
+    }
+}
